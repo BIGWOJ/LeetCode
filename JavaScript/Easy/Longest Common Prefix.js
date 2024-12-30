be69ec8c292abbaf4555781d9e176cc0d9141e98
BIGWOJ <wojtek.latos@interia.pl>
2024-12-30 23:03:11 +0100

    Longest Common Prefix, Palindrome Number, Roman to Integer, Two Sum

---
+++ JavaScript/Easy/Longest Common Prefix.js
@@ -0,0 +1,38 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+//
+// If there is no common prefix, return an empty string ""
+// Example 1:
+//
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+//
+// Example 2:
+//
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+// Constraints:
+//
+//     1 <= strs.length <= 200
+//     0 <= strs[i].length <= 200
+//     strs[i] consists of only lowercase English letters.
+//
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return ""
+    }
+
+    let shortest_word = strs.reduce((a, b) =>{
+        return a.length <= b.length ? a : b;
+    })
+
+    let longest_prefix = shortest_word;
+    for (let i = 0; i < shortest_word.length; i++) {
+        for (let word of strs) {
+            if (word[i] !== longest_prefix[i]) {
+                return longest_prefix.slice(0, i);
+            }
+        }
+    }
+    return longest_prefix;
+}
---
+++ Python/Easy/Longest Common Prefix.py
@@ -0,0 +1,48 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+#
+# If there is no common prefix, return an empty string ""
+# Example 1:
+#
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+#
+# Example 2:
+#
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+# Constraints:
+#
+#     1 <= strs.length <= 200
+#     0 <= strs[i].length <= 200
+#     strs[i] consists of only lowercase English letters.
+#
+
+class Solution:
+    #1st attempt
+    def longestCommonPrefix(self, strs):
+        longest_prefix = ""
+        if not strs:
+            longest_prefix
+        shortest_word = min(strs, key=len)
+        for char in shortest_word:
+            longest_prefix += char
+            for word in strs:
+                if word.startswith(longest_prefix):
+                    continue
+                else:
+                    longest_prefix = longest_prefix[:-1]
+                    break
+        return longest_prefix
+
+    #2nd attempt - faster
+    def longestCommonPrefix_2(self, strs):
+        if not strs:
+            return ""
+        shortest_word = min(strs, key=len)
+        longest_prefix = shortest_word
+        for i in range(len(shortest_word)):
+            for word in strs:
+                if word[i] != longest_prefix[i]:
+                    return longest_prefix[:i]
+        return longest_prefix
